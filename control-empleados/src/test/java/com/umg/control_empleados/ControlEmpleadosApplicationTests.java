c685f435ed98ca48e8fb1400b2c9e00feff5010f
Joab Ramirez <joab@localhost.localdomain>
2024-08-20 23:49:41 -0600

    First commit

---
+++ control-empleados/src/test/java/com/umg/control_empleados/ControlEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.control_empleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
